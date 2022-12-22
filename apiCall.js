5e578209c629cd4f4a01f75c7d7a9878848c24c7
chandra-shekr <chandrashekarshashidhara@gmail.com>
2022-12-22 12:23:17 +0530

    Chim Letter deploy

---
+++ apiCall.js
@@ -0,0 +1,97 @@
+//jshint esversion:6
+
+"use strict"
+
+import fetch from "node-fetch";
+import path from "path";
+
+function mailChimpAPI({ method, body = {}, listID = "", apiPath = "lists", section = "", memberID = "", actions = "" }) {
+
+    const Authorization = "Basic 317a36383ceb7565957fdebf1bbaeadf-us21";
+    const urlPath = path.join(apiPath, listID, section, memberID, actions)
+    let options, methodType;
+    switch (true) {
+        case method.toLowerCase() === "get":
+            methodType = method.toUpperCase();
+            options = {
+                "method": methodType,
+                redirect: "follow",
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization
+                },
+            }
+            break;
+        case method.toLowerCase() === "post" || method.toLowerCase() === "patch" || method.toLowerCase() === "delete" || method.toLowerCase() === "put":
+            methodType = method.toUpperCase();
+            options = {
+                "method": methodType,
+                redirect: "follow",
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization
+                },
+                body: JSON.stringify(body)
+
+            }
+            break;
+        default:
+            options = {}
+
+    }
+    const res = fetch(`https://us21.api.mailchimp.com/3.0/${urlPath}`, options)
+        .then(response => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                return new Promise((resolve, reject) => {
+                    response.json().then(val => {
+                        reject(new Error(JSON.stringify(val)));
+                    });
+                });
+            }
+
+        })
+        .then(result => {
+            return result;
+        })
+        .catch(err => {
+            return new Promise((resolve, reject) => {
+
+                reject(new Error(err.message));
+            })
+        });
+
+    return res;
+}
+
+let someData = {
+    status: "subscribed"
+}
+
+
+
+
+
+/*
+let createList = {
+    name: "name",
+    permission_reminder: "permission_reminder",
+    email_type_option: true,
+    contact: {
+        company: "company",
+        address1: "address1",
+        city: "city",
+        country: "country",
+        state: "New York",
+        zip: "10001"
+    },
+    campaign_defaults: {
+        from_name: "from_name",
+        from_email: "Beulah_Ryan@hotmail.com",
+        subject: "subject",
+        language: "language",
+    },
+}
+*/
+export { mailChimpAPI };
---
+++ app.js
@@ -0,0 +1,85 @@
+//jshint esversion:6
+
+"use strict"
+
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+import { mailChimpAPI } from "./apiCall.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+const port = 3999;
+
+// ############################ Middlewear <start>#####################################
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.join(dirname(__filename), "public");
+
+// In order for our server to serve up static files like .css, image files etc, we need to use 'express.static'
+app.use(express.static(__dirname));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.json({ type: ['application/json', 'text/plain'] }));
+
+// ############################# Middlewear <end>####################################
+
+app.get("/", (req, res) => {
+
+    console.log(req.cookies);
+    res.sendFile(path.join(__dirname, "signup.html"));
+})
+
+app.post("/", (req, res) => {
+    console.log(req.body.name, req.body.email);
+    let name = req.body.name.split(" ");
+    if (name.length > 1) {
+        var [firstName, lastName] = [name[0], name[name.length - 1]];
+    } else {
+        var [firstName, lastName] = [name[0], null];
+    }
+    let userData = {
+        members: [
+            {
+                email_address: req.body.email,
+                status: "pending",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName,
+                }
+            }
+        ]
+    }
+    const mailResult = mailChimpAPI({ method: "post", body: userData, listID: "53df500519" })
+        .then(result => {
+            console.log(result);
+            if (result.errors.length > 0) {
+                res.status(400).redirect("/failed");
+            } else {
+                res.status(200).redirect("/success");
+            }
+        })
+        .catch(err => {
+            console.log(err.message);
+            res.status(400).redirect("/failed");
+        })
+})
+
+app.get("/success", (req, res) => {
+    res.sendFile(path.join(__dirname, "success.html"));
+})
+
+app.get("/failed", (req, res) => {
+    res.sendFile(path.join(__dirname, "failure.html"));
+})
+
+app.post("/loc", (req, res) => {
+
+    console.log(req.body);
+    res.status(200).send({ msg: "geo-tagged" });
+})
+
+app.listen(port, () => {
+    console.log("Listening on port ", port);
+})
